3c71554df9358c981b78c3691d106af8f83c2369
ramibs1 <145760574+ramibs1@users.noreply.github.com>
2024-02-15 20:24:30 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
 print('hello world')
-x = int(input('donner x : '))
-for i in range (x) :
-  print('mahdi rajl')
+# hello.py
+
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide the value of x as a command-line argument.")
+        sys.exit(1)
+        
+    x = int(sys.argv[1])
+    print(f"x is {x}")
+
+if __name__ == "__main__":
+    main()
+
