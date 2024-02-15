4fcf51528bb2d54b6cab5e5cb92caf26594c7fff
ramibs1 <145760574+ramibs1@users.noreply.github.com>
2024-02-15 20:25:45 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,4 @@
 print('hello world')
-# hello.py
-
-import sys
-
-def main():
-    if len(sys.argv) < 2:
-        print("Please provide the value of x as a command-line argument.")
-        sys.exit(1)
-        
-    x = int(sys.argv[1])
-    print(f"x is {x}")
-
-if __name__ == "__main__":
-    main()
+for i in range (10):
+    print('My first try!!!')
 
